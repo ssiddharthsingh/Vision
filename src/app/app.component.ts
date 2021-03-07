8b7a6ebea0bf42dc06cc66e08b87220d191d14de
siddharth singh <sidsingh946179@gmail.com>
2021-03-07 14:30:55 +0530

    vi

---
+++ src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import {Todo} from"./todo";
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'VISION';
+ 
+  todoValue:string;
+ list: Todo[];
+
+ngOnInit(){
+  
+}
+
+}
